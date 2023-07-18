7335916d8c122c6981ad90fb53254d3650ad8e2a
Travis Simpson <Travis.Simpson@fdmgroup.com>
2023-07-18 17:35:34 +0100

    minor styling

---
+++ blog-front-end/src/Footer.js
@@ -0,0 +1,28 @@
+const Footer = () => (
+    <footer class="footer">
+        <div class="footer-content">
+            <div class="footer-column">
+                <h4>About Us</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+            <div class="footer-column">
+                <h4>Contact</h4>
+                <p>Email: info@example.com</p>
+                <p>Phone: 123-456-7890</p>
+            </div>
+            <div class="footer-column">
+                <h4>Follow Us</h4>
+                <div class="social-media">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <p>&copy; 2023 Your Website. All rights reserved.</p>
+        </div>
+    </footer>
+) 
+
+export default Footer;
