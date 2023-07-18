dea3212fcda97f4236f5812e8351312ea31e9056
Travis Simpson <Travis.Simpson@fdmgroup.com>
2023-07-18 18:03:16 +0100

    fix(navbar): current page changes when using browser back/forward button

--- blog-front-end/src/NavBar.js
+++ blog-front-end/src/NavBar.js
@@ -1,31 +1,54 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useLocalState } from "./hooks/useLocalStorage";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const NavBar = () => {
-    const [activePage, setActivePage] = useState('home');
-    const navigate = useNavigate();
-    const [jwt, setJwt] = useLocalState("", "jwt");
-    const logout = () => {
-        localStorage.removeItem("jwt");
-        setJwt("");
-        window.location.href = "/";;
-    }
-    return(
-        <nav className="navbar">
-            <ul>
-                <li className={activePage === 'home' ? 'active' : ''}>
-                    <Link to="/" onClick={() => setActivePage('home')}>Home</Link>
-                </li>
-                <li className={activePage === 'about' ? 'active' : ''}>
-                    <Link to="/about" onClick={() => setActivePage('about')}>About</Link>
-                </li>
-            </ul>
-            <div className="nav-button">
-                {jwt === "" ? <button onClick={() => navigate('/login')}>Log in</button> : <button onClick={() => logout()}>Log out</button>}
-            </div>
-        </nav>
-    )
-}
+  const [activePage, setActivePage] = useState("home");
+  const [jwt, setJwt] = useLocalState("", "jwt");
+  const location = useLocation();
+
+  const handleNavigation = (page) => {
+    setActivePage(page);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("jwt");
+    setJwt("");
+    window.location.href = "/";
+  };
+
+  useEffect(() => {
+    const path = location.pathname.substring(1);
+    setActivePage(path || "home");
+  }, [location]);
+
+  return (
+    <nav className="navbar">
+      <ul>
+        <li className={activePage === "home" ? "active" : ""}>
+          <Link to="/" onClick={() => handleNavigation("home")}>
+            Home
+          </Link>
+        </li>
+        <li className={activePage === "about" ? "active" : ""}>
+          <Link to="/about" onClick={() => handleNavigation("about")}>
+            About
+          </Link>
+        </li>
+        <li className={activePage === "login" ? "active nav-right" : "nav-right"}>
+          {jwt === "" ? (
+            <Link to="/login" onClick={() => handleNavigation("login")}>
+              Log in
+            </Link>
+          ) : (
+            <Link to="/" onClick={logout}>
+              Log out
+            </Link>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default NavBar;
