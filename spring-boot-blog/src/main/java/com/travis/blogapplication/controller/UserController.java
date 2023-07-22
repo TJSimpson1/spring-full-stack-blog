0abf44287ac3077845359ac32214b672640a3f45
Travis Simpson <Travis.Simpson@fdmgroup.com>
2023-07-22 18:48:57 +0100

    Refactoring

--- spring-boot-blog/src/main/java/com/travis/blogapplication/controller/UserController.java
+++ spring-boot-blog/src/main/java/com/travis/blogapplication/controller/UserController.java
@@ -3,11 +3,8 @@ package com.travis.blogapplication.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,20 +23,14 @@ public class UserController {
 	private UserService userService;
 	
 	@GetMapping("/{username}")
-	public ResponseEntity<User> getUserByUsername(@PathVariable("username") String username) {
-	    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-	    if (authentication != null && authentication.isAuthenticated()) {
-	        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-	        if (username.equals(userDetails.getUsername())) {
-	            Optional<User> user = userService.findUserByUsername(username);
-	            if (user.isPresent()) {
-	                return ResponseEntity.ok(user.get());
-	            }
-	            return ResponseEntity.notFound().build();
-	        }
-	    }
-	    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-	}
+    @PreAuthorize("isAuthenticated() and (#username == authentication.principal.username or hasRole('ADMIN'))")
+    public ResponseEntity<User> getUserByUsername(@PathVariable("username") String username) {
+        Optional<User> user = userService.findUserByUsername(username);
+        if (user.isPresent()) {
+            return ResponseEntity.ok(user.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
 	
 	@GetMapping("/{username}/role")
 	public ResponseEntity<String> getUserRoleByUsername(@PathVariable String username){
