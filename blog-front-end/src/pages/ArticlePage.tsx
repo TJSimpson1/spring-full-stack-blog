f6dd7108385bfc37bfd37a2476e87c1dc35ea441
Travis Simpson <Travis.Simpson@fdmgroup.com>
2023-08-08 17:29:22 +0100

    Minor changes

--- blog-front-end/src/pages/ArticlePage.tsx
+++ blog-front-end/src/pages/ArticlePage.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 interface Article {
     id: number;
@@ -15,14 +16,17 @@ interface Article {
 const ArticlePage: React.FC = () => {
     const { articleId } = useParams<{ articleId: string }>();
     const [article, setArticle] = useState<Article | undefined>();
+    const [isLoading, setIsLoading] = useState<boolean>(true);
 
     useEffect(() => {
         const fetchArticle = async () => {
             try {
                 const response = await axios.get<Article>(`http://localhost:8080/api/articles/${articleId}`);
                 setArticle(response.data);
+                setIsLoading(false);
             } catch (error) {
                 console.error("Failed to fetch article", error);
+                setIsLoading(false);
             }
         };
 
@@ -31,14 +35,18 @@ const ArticlePage: React.FC = () => {
 
     return (
         <div className="article-page">
-            {article && (
-                <div className="article">
-                    <h1>{article.title}</h1>
-                    <p>Author: {article.author.name}</p>
-                    {article.content.map((paragraph, i) => (
-                        <p key={i}>{paragraph}</p>
-                    ))}
-                </div>
+            {isLoading ? (
+                <LoadingSpinner />
+            ) : (
+                article && (
+                    <div className="article">
+                        <h1>{article.title}</h1>
+                        <p>Author: {article.author.name}</p>
+                        {article.content.map((paragraph, i) => (
+                            <p key={i}>{paragraph}</p>
+                        ))}
+                    </div>
+                )
             )}
         </div>
     );
--- blog-front-end/src/pages/ArticlesListPage.tsx
+++ blog-front-end/src/pages/ArticlesListPage.tsx
@@ -1,29 +1,40 @@
 import React, { useEffect, useState } from "react";
 import ArticlesList from "../components/ArticlesList";
 import axios from "axios";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const ArticlesListPage = () => {
-    const [articles, setArticles] = useState([]);
+  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
-    useEffect(() => {
-        const fetchArticles = async () => {
-            try {
-                const response = await axios.get("http://localhost:8080/api/articles");
-                setArticles(response.data);
-            } catch (error) {
-                console.error("Failed to fetch articles", error);
-            }
-        };
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+        const response = await axios.get("http://localhost:8080/api/articles");
+        setArticles(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Failed to fetch articles", error);
+        setIsLoading(false);
+      }
+    };
 
-        fetchArticles();
-    }, []);
+    fetchArticles();
+  }, []);
 
-    return (
-        <>
-            <h1>Articles</h1>
-            <ArticlesList articles={articles} />
-        </>
-    );
+  return (
+    <div>
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <div>
+          <h1>Articles</h1>
+          <ArticlesList articles={articles} />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ArticlesListPage;
