57e83f6dcb677a401df0f9375900b4cba4fc9668
Travis Simpson <Travis.Simpson@fdmgroup.com>
2023-08-08 17:44:13 +0100

    Formatting

--- blog-front-end/src/pages/AdminPage.tsx
+++ blog-front-end/src/pages/AdminPage.tsx
@@ -1,16 +1,63 @@
 import React from "react";
 
 const AdminPage = () => {
-    return (
-        <>
-        <h1>Welcome to the admin page</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur lectus id augue gravida, in volutpat ligula posuere. Sed congue, urna sed aliquet eleifend, turpis odio vehicula libero, nec dapibus orci arcu a lectus. Morbi auctor tortor non ipsum blandit, sed aliquam est ultrices. Fusce gravida est ac malesuada iaculis. Aliquam dignissim auctor libero, ut dapibus ligula bibendum eu. Nam pharetra justo vel finibus suscipit. Praesent rutrum ligula at tortor rhoncus viverra. Suspendisse egestas finibus feugiat. Vivamus ultrices ante sit amet justo dignissim ultrices. Mauris et arcu felis. Integer ut condimentum leo, nec blandit ex. Donec consequat varius quam, sit amet eleifend mi tempus eget.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur lectus id augue gravida, in volutpat ligula posuere. Sed congue, urna sed aliquet eleifend, turpis odio vehicula libero, nec dapibus orci arcu a lectus. Morbi auctor tortor non ipsum blandit, sed aliquam est ultrices. Fusce gravida est ac malesuada iaculis. Aliquam dignissim auctor libero, ut dapibus ligula bibendum eu. Nam pharetra justo vel finibus suscipit. Praesent rutrum ligula at tortor rhoncus viverra. Suspendisse egestas finibus feugiat. Vivamus ultrices ante sit amet justo dignissim ultrices. Mauris et arcu felis. Integer ut condimentum leo, nec blandit ex. Donec consequat varius quam, sit amet eleifend mi tempus eget.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur lectus id augue gravida, in volutpat ligula posuere. Sed congue, urna sed aliquet eleifend, turpis odio vehicula libero, nec dapibus orci arcu a lectus. Morbi auctor tortor non ipsum blandit, sed aliquam est ultrices. Fusce gravida est ac malesuada iaculis. Aliquam dignissim auctor libero, ut dapibus ligula bibendum eu. Nam pharetra justo vel finibus suscipit. Praesent rutrum ligula at tortor rhoncus viverra. Suspendisse egestas finibus feugiat. Vivamus ultrices ante sit amet justo dignissim ultrices. Mauris et arcu felis. Integer ut condimentum leo, nec blandit ex. Donec consequat varius quam, sit amet eleifend mi tempus eget.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur lectus id augue gravida, in volutpat ligula posuere. Sed congue, urna sed aliquet eleifend, turpis odio vehicula libero, nec dapibus orci arcu a lectus. Morbi auctor tortor non ipsum blandit, sed aliquam est ultrices. Fusce gravida est ac malesuada iaculis. Aliquam dignissim auctor libero, ut dapibus ligula bibendum eu. Nam pharetra justo vel finibus suscipit. Praesent rutrum ligula at tortor rhoncus viverra. Suspendisse egestas finibus feugiat. Vivamus ultrices ante sit amet justo dignissim ultrices. Mauris et arcu felis. Integer ut condimentum leo, nec blandit ex. Donec consequat varius quam, sit amet eleifend mi tempus eget.</p>
-        </> 
-    )
-    
-}
+  return (
+    <>
+      <h1>Welcome to the admin page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur
+        lectus id augue gravida, in volutpat ligula posuere. Sed congue, urna
+        sed aliquet eleifend, turpis odio vehicula libero, nec dapibus orci arcu
+        a lectus. Morbi auctor tortor non ipsum blandit, sed aliquam est
+        ultrices. Fusce gravida est ac malesuada iaculis. Aliquam dignissim
+        auctor libero, ut dapibus ligula bibendum eu. Nam pharetra justo vel
+        finibus suscipit. Praesent rutrum ligula at tortor rhoncus viverra.
+        Suspendisse egestas finibus feugiat. Vivamus ultrices ante sit amet
+        justo dignissim ultrices. Mauris et arcu felis. Integer ut condimentum
+        leo, nec blandit ex. Donec consequat varius quam, sit amet eleifend mi
+        tempus eget.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur
+        lectus id augue gravida, in volutpat ligula posuere. Sed congue, urna
+        sed aliquet eleifend, turpis odio vehicula libero, nec dapibus orci arcu
+        a lectus. Morbi auctor tortor non ipsum blandit, sed aliquam est
+        ultrices. Fusce gravida est ac malesuada iaculis. Aliquam dignissim
+        auctor libero, ut dapibus ligula bibendum eu. Nam pharetra justo vel
+        finibus suscipit. Praesent rutrum ligula at tortor rhoncus viverra.
+        Suspendisse egestas finibus feugiat. Vivamus ultrices ante sit amet
+        justo dignissim ultrices. Mauris et arcu felis. Integer ut condimentum
+        leo, nec blandit ex. Donec consequat varius quam, sit amet eleifend mi
+        tempus eget.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur
+        lectus id augue gravida, in volutpat ligula posuere. Sed congue, urna
+        sed aliquet eleifend, turpis odio vehicula libero, nec dapibus orci arcu
+        a lectus. Morbi auctor tortor non ipsum blandit, sed aliquam est
+        ultrices. Fusce gravida est ac malesuada iaculis. Aliquam dignissim
+        auctor libero, ut dapibus ligula bibendum eu. Nam pharetra justo vel
+        finibus suscipit. Praesent rutrum ligula at tortor rhoncus viverra.
+        Suspendisse egestas finibus feugiat. Vivamus ultrices ante sit amet
+        justo dignissim ultrices. Mauris et arcu felis. Integer ut condimentum
+        leo, nec blandit ex. Donec consequat varius quam, sit amet eleifend mi
+        tempus eget.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur
+        lectus id augue gravida, in volutpat ligula posuere. Sed congue, urna
+        sed aliquet eleifend, turpis odio vehicula libero, nec dapibus orci arcu
+        a lectus. Morbi auctor tortor non ipsum blandit, sed aliquam est
+        ultrices. Fusce gravida est ac malesuada iaculis. Aliquam dignissim
+        auctor libero, ut dapibus ligula bibendum eu. Nam pharetra justo vel
+        finibus suscipit. Praesent rutrum ligula at tortor rhoncus viverra.
+        Suspendisse egestas finibus feugiat. Vivamus ultrices ante sit amet
+        justo dignissim ultrices. Mauris et arcu felis. Integer ut condimentum
+        leo, nec blandit ex. Donec consequat varius quam, sit amet eleifend mi
+        tempus eget.
+      </p>
+    </>
+  );
+};
 
 export default AdminPage;
--- blog-front-end/src/pages/ArticlePage.tsx
+++ blog-front-end/src/pages/ArticlePage.tsx
@@ -4,52 +4,54 @@ import { useParams } from "react-router-dom";
 import LoadingSpinner from "../components/LoadingSpinner";
 
 interface Article {
-    id: number;
+  id: number;
+  name: string;
+  title: string;
+  content: string[];
+  author: {
     name: string;
-    title: string;
-    content: string[];
-    author: {
-        name: string;
-    };
+  };
 }
 
 const ArticlePage: React.FC = () => {
-    const { articleId } = useParams<{ articleId: string }>();
-    const [article, setArticle] = useState<Article | undefined>();
-    const [isLoading, setIsLoading] = useState<boolean>(true);
+  const { articleId } = useParams<{ articleId: string }>();
+  const [article, setArticle] = useState<Article | undefined>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
-    useEffect(() => {
-        const fetchArticle = async () => {
-            try {
-                const response = await axios.get<Article>(`http://localhost:8080/api/articles/${articleId}`);
-                setArticle(response.data);
-                setIsLoading(false);
-            } catch (error) {
-                console.error("Failed to fetch article", error);
-                setIsLoading(false);
-            }
-        };
+  useEffect(() => {
+    const fetchArticle = async () => {
+      try {
+        const response = await axios.get<Article>(
+          `http://localhost:8080/api/articles/${articleId}`
+        );
+        setArticle(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Failed to fetch article", error);
+        setIsLoading(false);
+      }
+    };
 
-        fetchArticle();
-    }, [articleId]);
+    fetchArticle();
+  }, [articleId]);
 
-    return (
-        <div className="article-page">
-            {isLoading ? (
-                <LoadingSpinner />
-            ) : (
-                article && (
-                    <div className="article">
-                        <h1>{article.title}</h1>
-                        <p>Author: {article.author.name}</p>
-                        {article.content.map((paragraph, i) => (
-                            <p key={i}>{paragraph}</p>
-                        ))}
-                    </div>
-                )
-            )}
-        </div>
-    );
+  return (
+    <div className="article-page">
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        article && (
+          <div className="article">
+            <h1>{article.title}</h1>
+            <p>Author: {article.author.name}</p>
+            {article.content.map((paragraph, i) => (
+              <p key={i}>{paragraph}</p>
+            ))}
+          </div>
+        )
+      )}
+    </div>
+  );
 };
 
 export default ArticlePage;
